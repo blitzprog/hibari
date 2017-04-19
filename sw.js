fb65d2b6c84749c2f83ced8d98791d5abef84918
wopian <wopian@wopian.me>
2017-04-19 12:19:13 +0000

    chore: deploy 608d2ca

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 var __wpo = {
   "assets": {
     "main": [
-      "/js/vendor.5487be35b3c3b93b81b2.js",
-      "/js/app.efe6519659dc523454a0.js",
-      "/js/manifest.2e4d929a9a6c9cf55d12.js",
+      "/js/vendor.b7048f09ad6de6252081.js",
+      "/js/app.3f10c24729ea09168806.js",
+      "/js/manifest.2bd7bad2e89a74002c74.js",
       "/css/app.3e8c55bfbf2f553f1f82a848dc2b6050.css",
-      "/js/app.efe6519659dc523454a0.js.gz",
+      "/js/app.3f10c24729ea09168806.js.gz",
       "/css/app.3e8c55bfbf2f553f1f82a848dc2b6050.css.gz",
-      "/js/vendor.5487be35b3c3b93b81b2.js.gz",
+      "/js/vendor.b7048f09ad6de6252081.js.gz",
       "/"
     ],
     "additional": [],
@@ -15,18 +15,18 @@ var __wpo = {
   },
   "externals": [],
   "hashesMap": {
-    "18c4a5f8e45d814dbff79553ebd4de87b96a37d2": "/js/vendor.5487be35b3c3b93b81b2.js",
-    "061fa8e4e2b5023120fb2aaca08b180694cc3b62": "/js/app.efe6519659dc523454a0.js",
-    "e495d713eca7a3eb1e1540d0d3681258a8b693d4": "/js/manifest.2e4d929a9a6c9cf55d12.js",
+    "bb992bfc16c1e1421eeb20a739138fb3563293ba": "/js/vendor.b7048f09ad6de6252081.js",
+    "1ea9d23a387138c3a671c9cd8707ddacc9eeb17a": "/js/app.3f10c24729ea09168806.js",
+    "cbe83b486850d375f28a15bf6169fbeb117298b6": "/js/manifest.2bd7bad2e89a74002c74.js",
     "c1023cf76e92262f460f7086cbad51bf97acd413": "/css/app.3e8c55bfbf2f553f1f82a848dc2b6050.css",
-    "817546846e13c65d942a19f9eef6d0863843b850": "/js/app.efe6519659dc523454a0.js.gz",
+    "0af499be3c8b915a02797bcfecccbd3cac55b478": "/js/app.3f10c24729ea09168806.js.gz",
     "4e92649239954f18f2e71023e258fe1633480971": "/css/app.3e8c55bfbf2f553f1f82a848dc2b6050.css.gz",
-    "d9ec63f226e785e9d53a76f0615e155c9aee7bda": "/js/vendor.5487be35b3c3b93b81b2.js.gz",
-    "847d4d2033a18b4760cd78168d067aa7c533139c": "/"
+    "bd9999020293fd60a7e1b3ace04783c02558f750": "/js/vendor.b7048f09ad6de6252081.js.gz",
+    "e04084cfd0489d2b459a58d4e1f54c0dac1379d3": "/"
   },
   "strategy": "changed",
   "responseStrategy": "network-first",
-  "version": "4/18/2017, 5:45:18 AM",
+  "version": "4/19/2017, 12:18:54 PM",
   "name": "webpack-offline",
   "pluginVersion": "4.7.0",
   "relativePaths": false
